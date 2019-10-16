25fd1bad611426ea2d0439ce3936f7de2eb0394a
yashbakshi-10 <55977623+yashbakshi-10@users.noreply.github.com>
2019-10-17 03:10:16 +0530

    first commit

---
+++ app/src/main/java/com/yash/workdesk/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package com.yash.workdesk.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
